8d74552b6ca3789f4ab324e559b42e73227e67b1
John Hao <johnhaoallwood@gmail.com>
2025-01-18 16:07:56 -0500

    changes

--- frontend.py
+++ frontend.py
@@ -6,4 +6,8 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/messages.html")
+def messages():
+    return render_template('messages.html')
+
 app.run()
